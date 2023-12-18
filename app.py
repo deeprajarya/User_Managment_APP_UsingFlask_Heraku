29f489c37c2c3690452a8ae7ecdd635dbb53cf7c
Deepraj Arya <mailforarya000@gmail.com>
2023-12-18 17:28:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # SQLite for simplicity
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    hobbies = db.Column(db.String(100))
+
+
+
+
+@app.route('/')
+def index():
+    with app.app_context():
+        users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        with app.app_context():
+            name = request.form['name']
+            phone_number = request.form['phone_number']
+            email = request.form['email']
+            hobbies = request.form['hobbies']
+
+            new_user = User(name=name, phone_number=phone_number, email=email, hobbies=hobbies)
+            db.session.add(new_user)
+            db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+
+
+@app.route('/delete/<int:user_id>')
+def delete(user_id):
+    with app.app_context():
+        user = User.query.get(user_id)
+        db.session.delete(user)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+
+@app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
+def edit_user(user_id):
+    with app.app_context():
+        user = User.query.get(user_id)
+
+        if request.method == 'POST':
+            user.name = request.form['name']
+            user.phone_number = request.form['phone_number']
+            user.email = request.form['email']
+            user.hobbies = request.form['hobbies']
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', user=user)
+
+
+
+# ... (Your existing code)
+
+@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit(user_id):
+    with app.app_context():
+        user = User.query.get(user_id)
+
+        if request.method == 'POST':
+            user.name = request.form['name']
+            user.phone_number = request.form['phone_number']
+            user.email = request.form['email']
+            user.hobbies = request.form['hobbies']
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', user=user)
+
+
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Replace with your email provider
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'EMAIL_ID'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'GMAIL_PASSWORD'  # Replace with your email password
+
+mail = Mail(app)
+
+
+
+@app.route('/send_email/<int:user_id>')
+def send_email(user_id):
+    with app.app_context():
+        user = User.query.get(user_id)
+        send_email_to_user(user)
+    
+    return redirect(url_for('index'))
+
+
+
+def send_email_to_user(user):
+    msg = Message('Subject: User Information',
+                  sender='deeprajarya11@example.com',  #  You can replace with your email
+                  recipients=[user.email])
+    msg.body = f"Hello {user.name},\n\nYour user information:\nName: {user.name}\nPhone Number: {user.phone_number}\nEmail: {user.email}\nHobbies: {user.hobbies}"
+    
+    mail.send(msg)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ readme.md
@@ -0,0 +1,62 @@
+# Project Title
+
+User Management App
+
+## Description
+
+This project is a simple web application for managing user information. It uses Flask for the backend, SQLAlchemy for database operations, and a basic HTML form for the frontend.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshots](#screenshots)
+- [Deployment](#deployment)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/user-management-app.git
+   cd user-management-app
+
+2. Create a virtual environment (recommended):
+   ```
+   conda create -p env python=3.8        # create virtual env
+   conda activate ./env                  # activate the virtual env
+
+
+3. Install Dependencies
+   ``` 
+   pip install -r requirements.txt
+   ```
+
+## Usages
+
+1. Run the Flask application:
+   ``` 
+   python app.py
+   ```
+The application will be accessible at http://127.0.0.1:5000/ in your web browser.
+
+2. Interact with the application:
+
+Visit the root route (/) to view the user information table.
+Access the /add route to add new users.
+
+
+## Screenshots
+
+![Screenshot 1](screenshots/Screenshot%20(129).png)
+*Caption: Front-end .*
+
+![Screenshot 2](screenshots/Screenshot%20(130).png)
+*Caption: adding data to db.*
+
+
+## Contributing
+Contributions are welcome! Reach me out at mailforarya@gmail.com
+
